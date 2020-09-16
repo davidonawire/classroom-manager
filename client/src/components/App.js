d46540baa8f0d0a62379a98fd39720cdbd590722
David Adams <onethinline@gmail.com>
2020-09-16 13:51:02 -0700

    Add routing, rough CourseDetails logic

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 
 import Courses from './Courses';
+import CourseDetail from './CourseDetail';
 
 function App() {
   return (
-    <div id="root">
-      <div>
-        <Courses />
+    <Router>
+      <div id="root">
+        <div>
+          <Switch>
+            <Route exact path="/" component={Courses} />
+            <Route path="/courses/:id" render={(props) => (<CourseDetail id={props.match.params.id} />)} />
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- client/src/components/Course.js
+++ client/src/components/Course.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Course = ({title, id}) => {
   return (
-    <div className="grid-33"><a className="course--module course--link" href="course-detail.html">
+    <div className="grid-33"><a className="course--module course--link" href={'/courses/' + id}>
       <h4 className="course--label">Course</h4>
       <h3 className="course--title">{title}</h3>
     </a></div>
