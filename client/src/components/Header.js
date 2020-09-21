e09308d342c0a3035bac45ec0aedd3043b850712
David Adams <onethinline@gmail.com>
2020-09-21 08:10:57 -0700

    Add Sign In logic and header, Forbidden component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Context } from '../Context';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const { authenticatedUser: authUser } = useContext(Context);
   return (
     <>
       <div className="header">
         <div className="bounds">
           <h1 className="header--logo">Courses</h1>
-          <nav><Link className="signup" to="/signup">Sign Up</Link><Link className="signin" href="/signin">Sign In</Link></nav>
+          <nav>{authUser ?
+            <>
+              <span>Welcome, {authUser.firstName} {authUser.lastName}!</span>
+              <Link className="signout" to="/signout">Sign Out</Link>
+            </>
+            :
+            <>
+              <Link className="signup" to="/signup">Sign Up</Link>
+              <Link className="signin" to="/signin">Sign In</Link>
+            </>
+          }
+          </nav>
         </div>
       </div>
       <hr />
--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -1,15 +1,34 @@
 import React, { useState, useContext } from 'react';
 import { Context } from '../Context';
 import { Link } from 'react-router-dom';
+import InlineErrors from './InlineErrors';
 
-const UserSignIn = () => {
+const UserSignIn = (props) => {
   const { actions } = useContext(Context);
   const [emailAddress, setEmailAddress] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
   
   const handleSubmit = (event) => {
     event.preventDefault();
-    // actions.signIn(emailAddress, password);
+    const { from } = props.location.state || { from: { pathname: '/' } };
+    actions.signIn(emailAddress, password)
+      .then(user => {
+        if (user === null) {
+          setErrors(['Sign-in was unsuccessful']);
+        } else {
+          props.history.push(from);
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        this.props.history.push('/error');
+      });
+  }
+
+  const handleCancel = (event) => {
+    event.preventDefault();
+    props.history.push('/');
   }
 
   return (
@@ -17,12 +36,13 @@ const UserSignIn = () => {
         <div className="grid-33 centered signin">
           <h1>Sign In</h1>
           <div>
+            <InlineErrors errors={errors} />
             <form onSubmit={handleSubmit}>
               <div><input id="emailAddress" name="emailAddress" type="text" placeholder="Email Address" value={emailAddress} onChange={e => setEmailAddress(e.target.value)} /></div>
               <div><input id="password" name="password" type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} /></div>
               <div className="grid-100 pad-bottom">
                 <button className="button" type="submit">Sign In</button>
-                <button className="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button>
+                <button className="button button-secondary" onClick={handleCancel}>Cancel</button>
               </div>
             </form>
           </div>
